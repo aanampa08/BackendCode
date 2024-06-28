f7ae58a5a85e8ca29ab40acdb2d5fc405e7208f4
aanampa08 <aliianampa2003@gmail.com>
2024-06-28 16:57:26 -0300

    Insertando nueva informacion de conciertos a la tabla conciertos

--- backend/mysqlScripts/insertaInfoConciertos.sql
+++ backend/mysqlScripts/insertaInfoConciertos.sql
@@ -1,8 +1,10 @@
 USE TicketCode;
+
 INSERT INTO Concierto(
     Nombre
     ,Artista
     ,Imagen
+    ,Descripcion
     ,Direccion
     ,Fecha
 ) 
@@ -10,6 +12,7 @@ VALUES (
 	'Antes De Que Amanezca: Tour Los Estadios'
     ,'Morat'
     ,'https://i.postimg.cc/kX7HYMZp/concierto-Morat.jpg'
+    ,'Morat, la banda que ha cautivado corazones en todo el mundo,presenta su nueva gira “Morat: Los Estadios". Antes de Que Amanezca”, unagira que promete no solo romper récords, sino también marcar un hito en lahistoria musical.'
     ,'Estadio Velez Sarsfield, Buenos Aires, Argentina'
     ,'2024-11-09 20:30:00'
 );
@@ -19,6 +22,7 @@ INSERT INTO Concierto(
     Nombre
     ,Artista
     ,Imagen
+    ,Descripcion
     ,Direccion
     ,Fecha
 ) 
@@ -26,8 +30,181 @@ VALUES (
 	'Miranda!'
     ,'Miranda'
     ,'https://i.postimg.cc/wx4NZ0bY/concierto-Miranda.png'
+    ,'Miranda! se presenta en el escenario del Movistar Arena para brindar varios shows luego de haber realizado múltiples presentaciones con su último disco Hotel Miranda! en 2023. El dúo apuesta a un show extravagante, alegre y disruptivo para celebrar 20 años de carrera'
     ,'Movistar Arena, Humboldt 450, Buenos Aires'
     ,'2024-11-09 22:00:00'
 );
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'Emilia MP3 TOUR'
+,'Emilia'
+,'https://i.postimg.cc/SQ7t2sRX/image.png'
+,'Emilia redobla la apuesta y anuncia una segunda función el día 13 de Octubre en el Estadio Vélez Sarsfield, en lo que será un hito sin precedente en su brillante y ascendente carrera.'
+,'Estadio Velez Sarsfield, Buenos Aires, Argentina'
+,'2024-10-13'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'Lo que hace el río'
+,'María Marull, Paula Marull, William Prociuk, Mónica Raiola, Mariano Saborido, Débora Zanolli'
+,'https://i.postimg.cc/CLGKbqXV/lo-que-el-rio-hace.jpg'
+,'El suceso teatral del año. La obra récord de público en la calle Corrientes, que hizo reír y emocionar a todos los espectadores, con premios de Argentina, Francia y España, con doce nominaciones a los premios ACE y ganadora de once premios nacionales ACE, Trinidad Guevara, María Guerrero y EEBA, vuelve al Teatro Astros.'
+,'Teatro Astros, Av.Corrientes 746 - CABA'
+,'2024-06-28 20:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'Cerrando ciclos'
+,'Aventura'
+,'https://i.postimg.cc/3N2jmFPv/feed-1024x856.jpg' 
+,'Aventura en Argentina: Los reyes de la bachata anuncian su segundo show en el Estadio Velez Sarsfield'
+,'Estadio Velez Sarsfield, Buenos Aires, Argentina'
+,'2024-10-23 21:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'La Última Noche Mágica: Desde cerca'
+,'Tan Bionica'
+,'https://i.postimg.cc/6Qw1by9R/image.png'
+,'Tan Bionica agrega una noche mágica más: el show llega, con producción de DF Entertainment. Para este show, a diferencia de los que la banda realizó el año pasado en estadios de más de 60.000 personas, Chano, Bambi, Seby y Diega eligieron un espacio que les permita lograr una conexión más íntima y cercana con sus fans'
+,'Movistar Arena, Humboldt 450, Buenos Aires'
+,'2024-08-18 21:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'Ciro y Los Persas'
+,'Ciro y Los Persas'
+,'https://i.postimg.cc/Y0zSgdzZ/ciro-1110x400.jpg'
+,'Luego de cerrar 2023 con tres funciones agotadas, Ciro y los Persas regresan al Movistar Arena el 8 de agosto. Será la tercera ocasión en que se presenten en este estadio de la Ciudad de Buenos Aires.'
+,'Movistar Arena, Humboldt 450, Buenos Aires'
+,'2024-08-08 21:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'QLokura'
+,'Nicolás Sattler, Facundo Herrera'
+,'https://i.postimg.cc/W1HHmrS1/image.png'
+,'Q`LOKURA VUELVE AL MOVISTAR ARENA DOMINGO 11 DE AGOSTO. En medio de su gira por España, la banda de cuarteto Q`LOKURA anuncia su tercer show en el Movistar Arena, el domingo 11 de agosto.'
+,'Movistar Arena, Humboldt 450, Buenos Aires'
+,'2024-08-11 21:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'51 años clásicos'
+,'Los Palmeras'
+,'https://i.postimg.cc/FK3rS9m1/image.png'
+,'LA MÍTICA BANDA VUELVE UNA VEZ MÁS AL MOVISTAR ARENA PARA CELEBRAR 51 AÑOS DE CLÁSICOS EL 13 DE SEPTIEMBRE. El emblemático grupo de cumbia santafesina no se detienen tras décadas de éxito logrando el sueño americano, rompiendo marcas y abriendo mercados impensados.'
+,'Movistar Arena, Humboldt 450, Buenos Aires'
+,'2024-09-13 21:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'Tour Lámina Once 2024'
+,'Cuarteto De Nos'
+,'https://i.postimg.cc/x8Sff8Z9/image.jpg'
+,'El público de Argentina se hizo escuchar y cuando comenzaron los anuncios de fechas, reclamaron el suyo.'
+,'Movistar Arena, Humboldt 450, Buenos Aires'
+,'2024-09-01 21:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'Único Show'
+,'Las Pastillas Del Abuelo'
+,'https://i.postimg.cc/nrkGTjbz/image.jpg'
+,'La banda de rock más exitosa de Argentina, Las Pastillas del Abuelo, continúa su año a lo grande en el Movistar Arena. Tras un comienzo de año espectacular frente a 35,000 personas y agotando todas las localidades en el Estadio Ferro, siendo el show más convocante de su historia, su gira por Latinoamérica y España con entradas totalmente agotadas los lleva al Movistar Arena, estableciéndose como una de las bandas más importantes del país. Con su espíritu y convicciones de siempre, siguen recorriendo este camino sin parar de crecer y crear, siempre acompañados por su fiel público. '
+,'Movistar Arena, Humboldt 450, Buenos Aires'
+,'2024-09-20 21:00:00'
+);
+
+INSERT INTO Concierto(
+	Nombre
+    ,Artista
+    ,Imagen
+    ,Descripcion
+    ,Direccion
+    ,Fecha
+)
+VALUES(
+'Flor Bertotti En Concierto'
+,'Florencia Bertotti'
+,'https://i.postimg.cc/c4vp9w42/flor-bertotti-1100x400.jpg'
+,'La gira, que ha sido un rotundo éxito con múltiples sold outs en diversas ciudades, ahora se prepara para su presentación inolvidable en el Movistar Arena. Flor Bertotti en Concierto, promete llevarte en un viaje musical lleno de emoción y alegría, recordando sus grandes éxitos y presentando material fresco que cautivará a todos los fanáticos de la talentosa actriz y cantante.'
+,'Movistar Arena, Humboldt 450, Buenos Aires'
+,'2024-09-21 21:00:00'
+);
+
+
 
 
