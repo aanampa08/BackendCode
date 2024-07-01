7bc4e205cd737c1e3104345ce3720b80fc5d7b86
aanampa08 <aliianampa2003@gmail.com>
2024-07-01 20:30:17 -0300

    Agrego script sql de cliente

---
+++ backend/mysqlScripts/creaTablaCliente.sql
@@ -0,0 +1,12 @@
+USE ticketcode;
+
+create table cliente(
+	idCliente INT AUTO_INCREMENT NOT NULL
+    ,Usuario VARCHAR(100) NOT NULL
+    ,Contraseña VARCHAR(100) NOT NULL
+    ,Dni INT NOT NULL
+    ,Nombre VARCHAR(100) NOT NULL
+    ,Apellido VARCHAR(100) NOT NULL
+    ,Mail VARCHAR(200) NOT NULL
+    ,Telefono INT NOT NULL
+);
