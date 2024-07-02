15b9ef76a9904b5c2110eff2efa2baaafbc38afa
aanampa08 <aliianampa2003@gmail.com>
2024-07-01 23:24:15 -0300

    Desarrollo createUser para poder insertar un nuevo cliente a la pagina

--- backend/controller/ticketCode.js
+++ backend/controller/ticketCode.js
@@ -12,7 +12,19 @@ const getAllConcerts = (req,res) =>{
     });
 }
 
+const createUser = (req,res) => {
+    console.log("llego al createUser");
+    const {usuario,contraseña,dni,nombre,apellido,mail,telefono} = req.body;
+    const sql = 'INSERT INTO cliente(Usuario,Contraseña,Dni,Nombre,Apellido,Mail,Telefono)VALUES(?,?,?,?,?,?,?)';
+    db.query(sql,[usuario,contraseña,dni,nombre,apellido,mail,telefono],(err, result) => {
+        if(err) throw err;
+        res.json({message:'Usuario creado.'});
+    });
+
+}
+
 // IMPORTANTE ENVIAR TODAS LAS PETICIONES QUE QUERRAMOS PROBAR
 module.exports={
     getAllConcerts
+    ,createUser
 };
