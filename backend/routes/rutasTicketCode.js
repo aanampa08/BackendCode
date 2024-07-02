468ec9faba9b7d7429669457465467d18cbf2f5b
aanampa08 <aliianampa2003@gmail.com>
2024-07-01 23:25:18 -0300

    Creacion del endpoint con metodo post para la insercion de clientes

--- backend/routes/rutasTicketCode.js
+++ backend/routes/rutasTicketCode.js
@@ -5,7 +5,6 @@ const ticketCode = require('../controller/ticketCode');
 
 //Generamos las rutas, y llamamos el desarrollo que va a realizar la peticion
 router.get('/',ticketCode.getAllConcerts);
-
-
+router.post('/usuario',ticketCode.createUser);
 
 module.exports=router;
