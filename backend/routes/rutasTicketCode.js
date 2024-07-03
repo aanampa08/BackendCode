981790b07027ed0b68121cf3a3845042dd59c8de
aanampa08 <aliianampa2003@gmail.com>
2024-07-02 23:55:52 -0300

    Creacion de las rutas get y post

--- backend/routes/rutasTicketCode.js
+++ backend/routes/rutasTicketCode.js
@@ -6,6 +6,7 @@ const ticketCode = require('../controller/ticketCode');
 //Generamos las rutas, y llamamos el desarrollo que va a realizar la peticion
 router.get('/',ticketCode.getAllConcerts);
 router.post('/usuario',ticketCode.createUser);
-console.log("HOLLAAAAA");
-router.get('/:usuario/:contraseña',ticketCode.searchUser);
+router.post('/login',ticketCode.searchUser);
+router.get('/:id',ticketCode.getConcertById);
+router.get('/compra',ticketCode.createCompra);
 module.exports=router;
