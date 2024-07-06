0973e35976b55ff87c55b0d9bb0a7ea33076b15f
aanampa08 <aliianampa2003@gmail.com>
2024-07-06 18:05:47 -0300

    Terminado el desarrollo de rutas para compras, tanto crear como traer

--- backend/controller/ticketCode.js
+++ backend/controller/ticketCode.js
@@ -48,12 +48,21 @@ const getConcertById=(req,res)=>{
     });
 }
 const createCompra = (req,res) => {
-    const {fecha,monto,usuario_id} = req.body;
+    const {fecha,monto,usuario_id,concierto_id,ticketsid,tickets_precio} = req.body;
     const sql = 'INSERT INTO compra(Fecha,Monto,usuario_id) VALUES (?,?,?)';
     db.query(sql,[fecha,monto,usuario_id],(err, result) => {
         if(err) throw err;
         
-        // res.json({estado:true});
+        const compra_id = result.insertId;
+        for(let i=0; i < ticketsid.length; i++){
+            const sql1='INSERT INTO detalle_compra(compra_id,concierto_id,ticket_id,precio_unitario) VALUES(?,?,?,?)';
+            db.query(sql1,[compra_id,concierto_id,ticketsid[i],tickets_precio[i]],(err, result) => {
+                if(err) throw err;
+            });
+        }
+        res.json({estado:true});
+
+       
     });
 }
 
@@ -68,6 +77,32 @@ const getAlltickets = (req,res) => {
     });
 }
 
+const getAllcompras = (req,res) => {
+    const {id} = req.params; 
+    const sql = 'select * from cliente,compra where cliente.idCliente=? and compra.usuario_id=?';
+    db.query(sql, [id,id], (err, resultado_compra) => {
+        if (err)throw err;
+        if(resultado_compra.length > 0){
+            const idCompra=resultado_compra[0].idCompra;
+            const monto=resultado_compra[0].Monto;
+            const fecha=resultado_compra[0].Fecha;
+            const sql_detalle='select compra_id,Nombre,sector,precio_unitario from ticket,detalle_compra,concierto where compra_id=? and ticket_id=idTicket and concierto.idConcierto=detalle_compra.concierto_id;';
+            
+            db.query(sql_detalle, [idCompra], (err, resultado_detalle_compra) => {
+                if (err)throw err;
+                
+                res.json({idCompra,monto,fecha,resultado_detalle_compra});
+
+
+                console.log("Compra encontrada.");
+            });
+        }
+        else{
+            res.json({mensaje:'No hay compras realizadas por el usuario'});
+        }
+       
+    });
+}
 
 // IMPORTANTE ENVIAR TODAS LAS PETICIONES QUE QUERRAMOS PROBAR
 module.exports={
@@ -77,4 +112,5 @@ module.exports={
     ,getConcertById
     ,createCompra
     ,getAlltickets
+    ,getAllcompras
 };
