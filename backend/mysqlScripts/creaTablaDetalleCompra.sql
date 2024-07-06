9dffb7415d6196b0c41613d9dbccbccf28e1e609
aanampa08 <aliianampa2003@gmail.com>
2024-07-06 18:04:51 -0300

    Insercion de un nuevo campo en la tabla de detalle de compra

--- backend/mysqlScripts/creaTablaDetalleCompra.sql
+++ backend/mysqlScripts/creaTablaDetalleCompra.sql
@@ -3,8 +3,10 @@ use ticketcode;
 CREATE TABLE detalle_compra(
 	idDetalleCompra INT NOT NULL AUTO_INCREMENT PRIMARY KEY
     ,compra_id INT
+    ,concierto_id INT 
     ,ticket_id INT
     ,precio_unitario INT
     ,FOREIGN KEY (compra_id) REFERENCES compra(idCompra)
+    ,FOREIGN KEY (concierto_id) REFERENCES concierto(idConcierto)
     ,FOREIGN KEY (ticket_id) REFERENCES ticket(idTicket)
 );
