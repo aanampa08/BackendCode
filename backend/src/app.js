f25b42f72d446c047b407a29add261c144a58a3b
aanampa08 <aliianampa2003@gmail.com>
2024-07-08 15:13:06 -0300

    Agrego importacion de cors cambiando el proxy

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,28 +10,18 @@ IMPORTANTE: el backend debe estar corriendo antes de ejecutar el frontend con el
 */  
 // creamos dependencia con express
 const express = require('express');
+const cors = require('cors');
 const rutasTicket= require('../routes/rutasTicketCode');
-
 const app = express();
 
-// Creacion de un proxy para evitar problemas con CORS Policity
-// + info: basicamente, al tener al cliente y servidor en distintos puertos se generan algunos inconvenientes
-// con este codigo creamos un Proxy el cual interactua como intermediario entre ambos.
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
+app.use(cors());
 //creamos dependencia para usar json
 app.use(express.json());
 //creamos middleare para gestionar las rutas que tengan
 app.use('/ticketCode',rutasTicket);
 
 //declaramos el puerto donde vamos a correr el back
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo exitosamente en el puerto: ${PORT}`);
-})
+});
